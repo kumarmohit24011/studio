a166fd136d5d81793466e561fe0ec3a0cc8be2bd
Mohit Kumar <kumarmohit24011@gmail.com>
2025-08-31 06:12:10 +0000

    # Add your Razorpay API Keys here. # You can get these from your Razorpa

--- src/app/checkout/actions.ts
+++ src/app/checkout/actions.ts
@@ -23,7 +23,7 @@ export async function createRazorpayOrder(amount: number) {
     }
 
     const razorpay = new Razorpay({
-        key_id: process.env.RAZORPAY_KEY_ID!,
+        key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
         key_secret: process.env.RAZORPAY_KEY_SECRET!,
     });
 
