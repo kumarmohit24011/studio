5ddf5d68d134dad04797a517428506e29de500f1
Mohit Kumar <kumarmohit24011@gmail.com>
2025-09-06 13:09:49 +0000

    ₹  is ot displaying correctlly symbol is display as changed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"Playfair Display"', 'serif'],
+        body: ['system-ui', 'sans-serif'],
+        headline: ['system-ui', 'serif'],
         code: ['monospace'],
       },
       colors: {
