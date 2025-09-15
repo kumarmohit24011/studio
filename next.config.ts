04ffbb2932416c32b5fbfa89c60e8d06b375fc15
Mohit Kumar <kumarmohit24011@gmail.com>
2025-09-15 17:48:17 +0000

    Access to fetch at 'https://studio--redbow-24723.asia-east1.hosted.app//

--- next.config.ts
+++ next.config.ts
@@ -19,9 +19,9 @@ const nextConfig: NextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // Or you can be more specific here
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Allow all origins
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
         ]
       }
     ];
